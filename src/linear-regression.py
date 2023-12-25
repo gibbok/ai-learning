7da3a844b7d7a2890f5c58fc0b8352fd2a06b5b3
Simone Poggiali <gibbok.coding@gmail.com>
2023-12-25 21:46:37 +0100

    improve example

--- src/linear-regression.py
+++ src/linear-regression.py
@@ -4,12 +4,14 @@ from tensorflow import keras
 from keras import Sequential
 from keras.layers import Dense
 
-model = Sequential([Dense(units=1, input_shape=[1])])
+l0 = Dense(units=1, input_shape=[1])
+model = Sequential([l0])
 model.compile(optimizer="sgd", loss="mean_squared_error")
 
 xs = np.array([1, 2, 3, 4, 5, 6], dtype=float)
-ys = np.array([2, 4, 6, 8, 10, 11], dtype=float)
+ys = np.array([3, 4, 5, 6, 7, 8], dtype=float)  # y = x+2
 
 model.fit(xs, ys, epochs=500)
 
-print(model.predict([10.0]))
+print(model.predict([7]))
+print("Here is what I learned: {}".format(l0.get_weights()))
