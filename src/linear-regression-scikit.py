30b1efe9fbf70d0a9aa7da759a1c7f8517ddb1c0
Simone Poggiali <gibbok.coding@gmail.com>
2023-12-26 19:30:34 +0100

    show chart

--- src/linear-regression-scikit.py
+++ src/linear-regression-scikit.py
@@ -1,4 +1,7 @@
 from sklearn.linear_model import LinearRegression
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # Given data
 X = [[1], [2], [3], [4], [5]]  # Features (independent variable)
@@ -6,6 +9,9 @@ y = [2, 3, 4, 5, 6]  # Target (dependent variable)
 
 # Initialize the Linear Regression model
 model = LinearRegression()
+
+# Creating a DataFrame from X and y
+data = pd.DataFrame({"X": [val[0] for val in X], "Y": y})
 
 # Fit the model using the given data
 model.fit(X, y)
@@ -26,5 +32,10 @@ for i, x in enumerate(X):
 
 # Predict a new value
 new_value = 8
-predicted_y = model.predict([[new_value]])
-print(predicted_y)
+predicted_y_new = model.predict([[new_value]])
+print(f"Predicted Y for new value {new_value}: {predicted_y_new}")
+
+# Plotting the regression line in a new window
+plt.figure()  # Create a new figure
+sns.regplot(x="X", y="Y", data=data)
+plt.show()  # Show the plot in a new window
