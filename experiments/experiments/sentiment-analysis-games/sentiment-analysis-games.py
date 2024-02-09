7cd0a823e19a44bdf63a92d8f0bbe878de7e81c3
Simone Poggiali <gibbok.coding@gmail.com>
2024-02-09 18:39:17 +0100

    save

--- experiments/experiments/sentiment-analysis-games/sentiment-analysis-games.py
+++ experiments/experiments/sentiment-analysis-games/sentiment-analysis-games.py
@@ -6,13 +6,11 @@ dir_path = "./experiments/sentiment-analysis-games/"
 # Read data
 df = pd.read_csv(os.path.join(dir_path, "data.csv"))
 
-# Clean data
+# Clean review data
+# trim, remove line breacks, remove line with length <= 2
 df = df.assign(review=df["review"].str.strip().str.replace(r"\n", "")).loc[
     df["review"].str.len() > 2
 ]
-
-# Clean data
-df["review"] = df["review"].str.strip().str.replace(r"\n", "")
 
 # Save cleaned data
 df.to_csv(os.path.join(dir_path, "data-clean.csv"), index=False)
