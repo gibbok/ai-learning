bb17ba3ccc2a33cfcdb5a1df8f5768eb150419af
Simone Poggiali <gibbok.coding@gmail.com>
2024-02-09 18:48:26 +0100

    save

--- experiments/experiments/sentiment-analysis-games/sentiment-analysis-games.py
+++ experiments/experiments/sentiment-analysis-games/sentiment-analysis-games.py
@@ -10,12 +10,11 @@ df = df[["review"]]
 # Strip leading and trailing whitespace
 df["review"] = df["review"].str.strip()
 # Remove line breaks
-df["review"] = df["review"].str.replace(r"\n", "")
+df["review"] = df["review"].str.replace(r"\n", " ", regex=True)
 # Filter rows with review length greater than 2
 df = df.loc[df["review"].str.len() > 2]
 # Remove duplicates
 df = df.drop_duplicates("review")
-
 
 # Save cleaned data
 df.to_csv(os.path.join(dir_path, "data-clean.csv"), index=False)
