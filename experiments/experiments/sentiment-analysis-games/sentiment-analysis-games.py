919812850ba07d05012ab111ae1282ce75aa1504
Simone Poggiali <gibbok.coding@gmail.com>
2024-02-09 16:36:24 +0100

    save

--- experiments/experiments/sentiment-analysis-games/sentiment-analysis-games.py
+++ experiments/experiments/sentiment-analysis-games/sentiment-analysis-games.py
@@ -2,7 +2,14 @@ import pandas as pd
 
 # Read data
 data = pd.read_csv("./experiments/sentiment-analysis-games/data.csv")
-
 reviews = data[["review"]]
 
-reviews_no_empties = reviews.dropna()
+# Drop empty rows
+reviews = reviews.dropna()
+reviews["review"] = reviews["review"].str.strip()
+
+# Remove any non-word or non-whitespace character
+special_chars = r"[^\w\s]"
+reviews["review"] = reviews["review"].str.replace(special_chars, "", regex=True)
+
+print(reviews)
