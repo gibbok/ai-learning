0bc17b04d8c5528b1c2c588bc0000a679a384cec
Simone Poggiali <gibbok.coding@gmail.com>
2024-02-09 18:40:55 +0100

    save

--- experiments/experiments/sentiment-analysis-games/sentiment-analysis-games.py
+++ experiments/experiments/sentiment-analysis-games/sentiment-analysis-games.py
@@ -12,5 +12,8 @@ df = df.assign(review=df["review"].str.strip().str.replace(r"\n", "")).loc[
     df["review"].str.len() > 2
 ]
 
+# Remove duplicates
+df = df.drop_duplicates("review")
+
 # Save cleaned data
 df.to_csv(os.path.join(dir_path, "data-clean.csv"), index=False)
