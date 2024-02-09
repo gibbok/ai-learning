e86781a27f43683d4e85480e7d89ddd200cb6bd0
Simone Poggiali <gibbok.coding@gmail.com>
2024-02-09 18:29:34 +0100

    save

--- experiments/experiments/sentiment-analysis-games/sentiment-analysis-games.py
+++ experiments/experiments/sentiment-analysis-games/sentiment-analysis-games.py
@@ -1,24 +1,18 @@
+import os
 import pandas as pd
 
 dir_path = "./experiments/sentiment-analysis-games/"
 
 # Read data
-df = pd.read_csv(dir_path + "data.csv")
+df = pd.read_csv(os.path.join(dir_path, "data.csv"))
 
-# Strip content
-df["review"] = df["review"].str.strip()
+# Clean data
+df = df.assign(review=df["review"].str.strip().str.replace(r"\n", "")).loc[
+    df["review"].str.len() > 2
+]
 
-# Drop empty rows
-df = df.dropna()
+# Clean data
+df["review"] = df["review"].str.strip().str.replace(r"\n", "")
 
-# Remove any line breaks
-special_chars = r"\n*"
-df["review"] = df["review"].str.replace(special_chars, "", regex=True)
-
-# Filter rows with one or two characters
-df = df[df["review"].apply(lambda x: len(x) > 2)]
-
-# Select only review
-# df = df[["review"]]
-
-df.to_csv(dir_path + "data-clean.csv", index=False)
+# Save cleaned data
+df.to_csv(os.path.join(dir_path, "data-clean.csv"), index=False)
