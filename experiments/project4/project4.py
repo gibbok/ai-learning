8c74805ec3c684ecf4e7f862ded57d1762ddc273
Simone Poggiali <gibbok.coding@gmail.com>
2024-02-23 15:53:32 +0100

    x

--- experiments/project4/project4.py
+++ experiments/project4/project4.py
@@ -7,16 +7,28 @@ os.system("clear")
 dir_path = "./experiments/project4/"
 df = pd.read_csv(os.path.join(dir_path, "data.csv"))
 
-# Remove rows with no country
+# Remove rows with no 'country'
 df = df.dropna(subset=["country"])
 
-
-# Split the 'listed_in' column into a list and then explode it into separate rows
+# Split columns with values as stringo into a list and then explode it into separate rows
 df = (
     df.assign(listed_in=df["listed_in"].str.split(", "))
     .explode("listed_in")
     .reset_index(drop=True)
 )
 
+df = (
+    df.assign(country=df["country"].str.split(", "))
+    .explode("country")
+    .reset_index(drop=True)
+)
+
+df = (
+    df.assign(director=df["director"].str.split(", "))
+    .explode("director")
+    .reset_index(drop=True)
+)
+
+df = df.assign(cast=df["cast"].str.split(", ")).explode("cast").reset_index(drop=True)
 
 df.to_csv(os.path.join(dir_path, "results.csv"), index=False)
