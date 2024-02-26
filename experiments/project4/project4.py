1fad3d079db73096911597e8216917c6c8db76cf
Simone Poggiali <gibbok.coding@gmail.com>
2024-02-26 17:02:29 +0100

    x

--- experiments/project4/project4.py
+++ experiments/project4/project4.py
@@ -5,7 +5,7 @@ os.system("clear")
 
 # Read
 dir_path = "./experiments/project4/"
-df = pd.read_csv(os.path.join(dir_path, "data.csv"))[0:200]
+df = pd.read_csv(os.path.join(dir_path, "data.csv"))[0:100]
 
 # Remove rows with no 'country'
 df = df.dropna(subset=["country"])
@@ -28,10 +28,13 @@ df = df.assign(director_new=df["director"].str.split(", ")).explode(
 df = df.assign(cast_new=df["cast"].str.split(", ")).explode(
     "cast_new", ignore_index=True
 )
-
+print(df)
+# Count how many categories we have
 grouped_data = df.groupby(["listed_in_new"]).count()
 
-print(grouped_data[["title"]])
+# print(grouped_data[["title"]])
 
-# df[0:200].to_csv(os.path.join(dir_path, "results.csv"), index=False)
-# grouped_data.to_csv(os.path.join(dir_path, "results_grouped.csv"), index=False)
+df.to_csv(os.path.join(dir_path, "results.csv"), index=False)
+grouped_data.to_csv(os.path.join(dir_path, "results_grouped.csv"), index=False)
+
+# Count how many categories we have for each country
