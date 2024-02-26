49cb3bc8e369f2c09813f304a54a1cdab85d8858
Simone Poggiali <gibbok.coding@gmail.com>
2024-02-26 16:31:44 +0100

    s

--- experiments/project4/test.py
+++ experiments/project4/test.py
@@ -10,7 +10,7 @@ data = {
 }
 df = pd.DataFrame(data)
 
-# Explode the "country" column to split the comma-separated values into separate rows
+# explode the "country" column to split the comma-separated values into separate rows
 df_expanded = df.assign(listed_in_new=df["listed_in"].str.split(", ")).explode(
     "listed_in_new", ignore_index=True
 )
@@ -19,8 +19,12 @@ print(df)
 print("-----------------")
 print(df_expanded[["title", "country", "listed_in_new"]])
 
-df_grouped = df_expanded.groupby(["listed_in_new"]).count()
-
-# ho2 many listed_in we have?
+# how many `listed_in` we have?
 print("-----------------")
-print(df_grouped)
+df_grouped_listed_in = df_expanded.groupby(["listed_in_new"]).count()
+print(df_grouped_listed_in)
+
+# list how many `listed_in` we have per `country`?
+df_grouped_country = df_expanded.groupby(["country"]).count()
+print("-----------------")
+print(df_grouped_listed_in)
