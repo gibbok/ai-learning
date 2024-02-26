a07fc8ecddac3c94fd6380f0d21ea7ed93f975e7
Simone Poggiali <gibbok.coding@gmail.com>
2024-02-26 16:54:03 +0100

    x

--- experiments/project4/project4.py
+++ experiments/project4/project4.py
@@ -12,28 +12,26 @@ df = df.dropna(subset=["country"])
 
 # Split columns with values as stringo into a list and then explode it into separate rows
 df = (
-    df.assign(listed_in=df["listed_in"].str.split(", "))
-    .explode("listed_in")
+    df.assign(listed_in_new=df["listed_in"].str.split(", "))
+    .explode("listed_in_new")
     .reset_index(drop=True)
 )
 
-df = (
-    df.assign(country=df["country"].str.split(", "))
-    .explode("country")
-    .reset_index(drop=True)
+df = df.assign(country_new=df["country"].str.split(", ")).explode(
+    "country_new", ignore_index=True
 )
 
-df = (
-    df.assign(director=df["director"].str.split(", "))
-    .explode("director")
-    .reset_index(drop=True)
+df = df.assign(director_new=df["director"].str.split(", ")).explode(
+    "director_new", ignore_index=True
 )
 
-df = df.assign(cast=df["cast"].str.split(", ")).explode("cast").reset_index(drop=True)
+df = df.assign(cast_new=df["cast"].str.split(", ")).explode(
+    "cast_new", ignore_index=True
+)
 
-grouped_data = df.groupby(["country", "title"]).size().reset_index(name="count")
+grouped_data = df.groupby(["country_new", "title"]).count()
 
 print(grouped_data)
 
-df.to_csv(os.path.join(dir_path, "results.csv"), index=False)
-# grouped_data.to_csv(os.path.join(dir_path, "results_grouped.csv"), index=False)
+# df[0:200].to_csv(os.path.join(dir_path, "results.csv"), index=False)
+grouped_data.to_csv(os.path.join(dir_path, "results_grouped.csv"), index=False)
