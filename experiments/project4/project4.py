df7dc970508b11edcde28fc786b490156a8c86db
Simone Poggiali <gibbok.coding@gmail.com>
2024-02-27 17:13:25 +0100

    x

--- experiments/project4/project4.py
+++ experiments/project4/project4.py
@@ -76,7 +76,22 @@ for category, titles in grouped_titles.items():
 
 print("------ Show number of TV snows compare to movies by year")
 # Group by release_year and count the occurrences of each type
-type_count_by_year = (
-    df.groupby(["release_year", "type"]).size().unstack(fill_value=0).transpose()
-)
-print(type_count_by_year)
+type_count_by_year = df.groupby(["release_year", "type"]).size().unstack(fill_value=0)
+type_count_by_year = type_count_by_year.reset_index()
+
+release_year = type_count_by_year["release_year"].to_list()
+
+plt.title("Number of Releases by Type Over Years")
+plt.xlabel("Year")
+plt.ylabel("Number of Releases")
+
+for type_value in type_count_by_year.columns:
+    if type_value == "release_year":
+        continue
+    count_values = []
+    for x in type_count_by_year[type_value]:
+        count_values.append(x)
+    plt.plot(release_year, count_values, label=type_value)
+
+plt.legend()
+plt.show()
