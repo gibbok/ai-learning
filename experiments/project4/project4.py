03cf82f67b3a39cb355314557a1e34bb091bc362
Simone Poggiali <gibbok.coding@gmail.com>
2024-02-27 17:22:06 +0100

    s

--- experiments/project4/project4.py
+++ experiments/project4/project4.py
@@ -12,21 +12,18 @@ df = pd.read_csv(os.path.join(dir_path, "data.csv"))
 # Remove rows with no 'country'
 df = df.dropna(subset=["country"])
 
-# Split columns with values as stringo into a list and then explode it into separate rows
+# Split columns with values as string into a list and then explode it into separate rows
 df = (
     df.assign(listed_in_new=df["listed_in"].str.split(", "))
     .explode("listed_in_new")
     .reset_index(drop=True)
 )
-
 df = df.assign(country_new=df["country"].str.split(", ")).explode(
     "country_new", ignore_index=True
 )
-
 df = df.assign(director_new=df["director"].str.split(", ")).explode(
     "director_new", ignore_index=True
 )
-
 df = df.assign(cast_new=df["cast"].str.split(", ")).explode(
     "cast_new", ignore_index=True
 )
@@ -45,7 +42,6 @@ for country in df["country"].unique():
     filtered_df = df[df["country"] == country]
     titles_country = filtered_df["title"].unique().tolist()
     temp_dict[country] = titles_country
-
 json_string = json.dumps(temp_dict, indent=4)
 print(json_string)
 
@@ -55,7 +51,6 @@ for country in df["country"].unique():
     filtered_df = df[df["country"] == country]
     listed_in_new_country = filtered_df["listed_in_new"].unique().tolist()
     temp_dict_listed_in_new[country] = listed_in_new_country
-
 json_string = json.dumps(temp_dict_listed_in_new, indent=4)
 print(json_string)
 
@@ -71,20 +66,17 @@ print("------ List all `title` in every `listed_in_new`")
 grouped_titles = df.groupby("listed_in_new")["title"].apply(pd.Series.unique)
 for category, titles in grouped_titles.items():
     print(f"Category: {category}")
-    print(*titles.tolist(), sep="\n")  # Print unique titles one per line
-    print("-" * 10)  # Print a separator for clarity
+    print(*titles.tolist(), sep="\n")
+    print("-" * 10)
 
-print("------ Show number of TV snows compare to movies by year")
-# Group by release_year and count the occurrences of each type
+print("------ Show number of TV snows compare to movies by year with a chart")
 type_count_by_year = df.groupby(["release_year", "type"]).size().unstack(fill_value=0)
 type_count_by_year = type_count_by_year.reset_index()
-
 release_year = type_count_by_year["release_year"].to_list()
 
 plt.title("Number of Releases by Type Over Years")
 plt.xlabel("Year")
 plt.ylabel("Number of Releases")
-
 for type_value in type_count_by_year.columns:
     if type_value == "release_year":
         continue
@@ -92,6 +84,5 @@ for type_value in type_count_by_year.columns:
     for x in type_count_by_year[type_value]:
         count_values.append(x)
     plt.plot(release_year, count_values, label=type_value)
-
 plt.legend()
 plt.show()
