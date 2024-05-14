45db36e34f79450d53c8b5524297a271d49e90c3
Simone Poggiali <gibbok.coding@gmail.com>
2024-05-14 16:02:26 +0200

    s

--- experiments/project6/project6.py
+++ experiments/project6/project6.py
@@ -1,117 +1,60 @@
-import os
+import matplotlib.pyplot as plt
 import numpy as np
-import keras
-from keras import layers
-from tensorflow import data as tf_data
+import PIL
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+import pathlib
 import matplotlib.pyplot as plt
 
-os.system("clear")
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file(
+    "flower_photos.tar", origin=dataset_url, extract=True
+)
+data_dir = pathlib.Path(data_dir).with_suffix("")
 
-print("hello world!")
+image_count = len(list(data_dir.glob("*/*.jpg")))
+print(image_count)
 
-# Filter out corrupted images
 
-num_skipped = 0
-for folder_name in ("Cat", "Dog"):
-    folder_path = os.path.join("./experiments/project6/PetImages", folder_name)
-    for fname in os.listdir(folder_path):
-        fpath = os.path.join(folder_path, fname)
-        try:
-            fobj = open(fpath, "rb")
-            is_jfif = b"JFIF" in fobj.peek(10)
-        finally:
-            fobj.close()
+batch_size = 32
+img_height = 180
+img_width = 180
 
-        if not is_jfif:
-            num_skipped += 1
-            # Delete corrupted image
-            os.remove(fpath)
-
-print(f"Deleted {num_skipped} images.")
-
-# Generate a Dataset
-image_size = (180, 180)
-batch_size = 128
-
-train_ds, val_ds = keras.utils.image_dataset_from_directory(
-    "./experiments/project6/PetImages",
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
     validation_split=0.2,
-    subset="both",
-    seed=1337,
-    image_size=image_size,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
     batch_size=batch_size,
 )
 
-# Using image data augmentation
-
-data_augmentation_layers = [
-    layers.RandomFlip("horizontal"),
-    layers.RandomRotation(0.1),
-]
-
-
-def data_augmentation(images):
-    for layer in data_augmentation_layers:
-        images = layer(images)
-    return images
-
-
-# Configure the dataset for performance
-# Apply `data_augmentation` to the training images.
-train_ds = train_ds.map(
-    lambda img, label: (data_augmentation(img), label),
-    num_parallel_calls=tf_data.AUTOTUNE,
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size,
 )
-# Prefetching samples in GPU memory helps maximize GPU utilization.
-train_ds = train_ds.prefetch(tf_data.AUTOTUNE)
-val_ds = val_ds.prefetch(tf_data.AUTOTUNE)
+
+class_names = train_ds.class_names
+print(class_names)
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 
-# Build a model
-def make_model(input_shape, num_classes):
-    inputs = keras.Input(shape=input_shape)
-
-    # Entry block
-    x = layers.Rescaling(1.0 / 255)(inputs)
-    x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-
-    previous_block_activation = x  # Set aside residual
-
-    for size in [256, 512, 728]:
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.Activation("relu")(x)
-        x = layers.SeparableConv2D(size, 3, padding="same")(x)
-        x = layers.BatchNormalization()(x)
-
-        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
-
-        # Project residual
-        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
-            previous_block_activation
-        )
-        x = layers.add([x, residual])  # Add back residual
-        previous_block_activation = x  # Set aside next residual
-
-    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-
-    x = layers.GlobalAveragePooling2D()(x)
-    if num_classes == 2:
-        units = 1
-    else:
-        units = num_classes
-
-    x = layers.Dropout(0.25)(x)
-    # We specify activation=None so as to return logits
-    outputs = layers.Dense(units, activation=None)(x)
-    return keras.Model(inputs, outputs)
+normalization_layer = layers.Rescaling(1.0 / 255)
 
 
-model = make_model(input_shape=image_size + (3,), num_classes=2)
-keras.utils.plot_model(model, show_shapes=True)
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+# Notice the pixel values are now in `[0,1]`.
+print(np.min(first_image), np.max(first_image))
