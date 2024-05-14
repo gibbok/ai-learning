886801b3010d7972eeaf61f7690420a97c2c3bf6
Simone Poggiali <gibbok.coding@gmail.com>
2024-05-14 19:33:14 +0200

    s

--- experiments/project6/use-model.py
+++ experiments/project6/use-model.py
@@ -1,11 +1,12 @@
 # Use the modal previously saved
 import tensorflow as tf
 
+# Image size for processing
 img_height = 180
 img_width = 180
 
+# Load image to predict
 img_path = "./experiments/project6/predict/5.jpg"
-
 img = tf.keras.utils.load_img(
     img_path,
     target_size=(img_height, img_width),
@@ -13,20 +14,15 @@ img = tf.keras.utils.load_img(
 img_array = tf.keras.utils.img_to_array(img)
 img_array = tf.expand_dims(img_array, 0)  # Create a batch
 
+# Load the model and make a prediction
 model = tf.keras.models.load_model("./experiments/project6/model.keras")
-
 predictions = model.predict(img_array)
 score = tf.nn.softmax(predictions[0])
 
-
-# Given labels, calculate percentages
-flowers = ["daisy", "dandelion", "roses", "sunflowers", "tulips"]
-
 # Pair each flower with its corresponding value
+flowers = ["daisy", "dandelion", "roses", "sunflowers", "tulips"]
 flower_value_pairs = list(zip(flowers, predictions[0]))
-
 sorted_flower_value_pairs = sorted(flower_value_pairs, key=lambda x: x[1], reverse=True)
-
 
 # Print results
 print("++++++++++++++++++++++++")
