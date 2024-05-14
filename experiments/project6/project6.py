c8559de65f272545058afad5c9fa5dae6c9f6efa
Simone Poggiali <gibbok.coding@gmail.com>
2024-05-14 15:42:19 +0200

    s

--- experiments/project6/project6.py
+++ experiments/project6/project6.py
@@ -8,3 +8,23 @@ import matplotlib.pyplot as plt
 os.system("clear")
 
 print("hello world!")
+
+# Filter out corrupted images
+
+num_skipped = 0
+for folder_name in ("Cat", "Dog"):
+    folder_path = os.path.join("./experiments/project6/PetImages", folder_name)
+    for fname in os.listdir(folder_path):
+        fpath = os.path.join(folder_path, fname)
+        try:
+            fobj = open(fpath, "rb")
+            is_jfif = b"JFIF" in fobj.peek(10)
+        finally:
+            fobj.close()
+
+        if not is_jfif:
+            num_skipped += 1
+            # Delete corrupted image
+            os.remove(fpath)
+
+print(f"Deleted {num_skipped} images.")
