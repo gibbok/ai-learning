64274bccabe82ca8aae7de0c9f276b22af75299f
Simone Poggiali <gibbok.coding@gmail.com>
2024-05-14 17:31:01 +0200

    s

--- experiments/project6/load-model.py
+++ experiments/project6/load-model.py
@@ -10,8 +10,14 @@ from tensorflow.keras.models import Sequential
 import pathlib
 import matplotlib.pyplot as plt
 
+batch_size = 32
 img_height = 180
 img_width = 180
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file(
+    "flower_photos.tar", origin=dataset_url, extract=True
+)
+data_dir = pathlib.Path(data_dir).with_suffix("")
 
 sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
 sunflower_path = tf.keras.utils.get_file("Red_sunflower", origin=sunflower_url)
@@ -25,5 +31,20 @@ model = keras.models.load_model("./experiments/project6/model.keras")
 predictions = model.predict(img_array)
 score = tf.nn.softmax(predictions[0])
 
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size,
+)
 
-print(score)
+class_names = train_ds.class_names
+num_classes = len(class_names)
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence.".format(
+        class_names[np.argmax(score)], 100 * np.max(score)
+    )
+)
