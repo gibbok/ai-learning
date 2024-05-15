bddaab337e07fd7c9b0f07954c7fd9302f6c941f
Simone Poggiali <17195702+gibbok@users.noreply.github.com>
2024-05-15 21:51:54 +0200

    Add comment

--- experiments/project6/project6.py
+++ experiments/project6/project6.py
@@ -1,4 +1,3 @@
-# Load data and generate the model
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
@@ -8,6 +7,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import pathlib
 
+# Use this script to load data and generate the model
 # Downloads dataset file from URL if it not already in the cache.
 dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
 data_dir = tf.keras.utils.get_file(
