c9a6f6c2d578aed166e4b380d77a125111be57f3
Simone Poggiali <gibbok.coding@gmail.com>
2024-05-15 20:41:30 +0200

    s

--- experiments/project7/project7.py
+++ experiments/project7/project7.py
@@ -1,140 +1,38 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import os
-import tensorflow as tf
+from tensorflow.keras.applications import MobileNet, imagenet_utils
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.models import Model
 
+# Define the input shape for the image
+img_shape = (224, 224, 3)
 
-_URL = "https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip"
-path_to_zip = tf.keras.utils.get_file("cats_and_dogs.zip", origin=_URL, extract=True)
-PATH = os.path.join(os.path.dirname(path_to_zip), "cats_and_dogs_filtered")
+# Load the pre-trained MobileNet model
+mobilenet = MobileNet(weights="imagenet", include_top=False, input_shape=img_shape)
 
-train_dir = os.path.join(PATH, "train")
-validation_dir = os.path.join(PATH, "validation")
+# Freeze the pre-trained model layers (not train them)
+for layer in mobilenet.layers:
+    layer.trainable = False
 
-BATCH_SIZE = 32
-IMG_SIZE = (160, 160)
+# Add a new classifier layer for our specific number of classes
+x = mobilenet.output
+predictions = Dense(10, activation="softmax")(x)  # Change 10 to your num_classes
 
-train_dataset = tf.keras.utils.image_dataset_from_directory(
-    train_dir, shuffle=True, batch_size=BATCH_SIZE, image_size=IMG_SIZE
-)
+# Create the final model
+model = Model(inputs=mobilenet.input, outputs=predictions)
 
-validation_dataset = tf.keras.utils.image_dataset_from_directory(
-    validation_dir, shuffle=True, batch_size=BATCH_SIZE, image_size=IMG_SIZE
-)
+# Compile the model (define optimizer and loss function)
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
+# Load and pre-process your image (replace with your image loading logic)
+img = image.load_img("./experiments/project7/predict/1.jpg", target_size=img_shape)
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)  # Add a batch dimension
+x = imagenet_utils.preprocess_input(x)  # Apply pre-processing for MobileNet
 
-class_names = train_dataset.class_names
+# Make prediction on the image
+predictions = model.predict(x)
 
-val_batches = tf.data.experimental.cardinality(validation_dataset)
-test_dataset = validation_dataset.take(val_batches // 5)
-validation_dataset = validation_dataset.skip(val_batches // 5)
-
-print(
-    "Number of validation batches: %d"
-    % tf.data.experimental.cardinality(validation_dataset)
-)
-print("Number of test batches: %d" % tf.data.experimental.cardinality(test_dataset))
-
-
-AUTOTUNE = tf.data.AUTOTUNE
-
-train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
-validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
-test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
-
-data_augmentation = tf.keras.Sequential(
-    [
-        tf.keras.layers.RandomFlip("horizontal"),
-        tf.keras.layers.RandomRotation(0.2),
-    ]
-)
-
-preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
-
-rescale = tf.keras.layers.Rescaling(1.0 / 127.5, offset=-1)
-
-
-# Create the base model from the pre-trained model MobileNet V2
-IMG_SHAPE = IMG_SIZE + (3,)
-base_model = tf.keras.applications.MobileNetV2(
-    input_shape=IMG_SHAPE, include_top=False, weights="imagenet"
-)
-
-
-image_batch, label_batch = next(iter(train_dataset))
-feature_batch = base_model(image_batch)
-print(feature_batch.shape)
-
-base_model.trainable = False
-
-base_model.summary()
-
-
-global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
-feature_batch_average = global_average_layer(feature_batch)
-print(feature_batch_average.shape)
-
-prediction_layer = tf.keras.layers.Dense(1, activation="sigmoid")
-prediction_batch = prediction_layer(feature_batch_average)
-print(prediction_batch.shape)
-
-
-inputs = tf.keras.Input(shape=(160, 160, 3))
-x = data_augmentation(inputs)
-x = preprocess_input(x)
-x = base_model(x, training=False)
-x = global_average_layer(x)
-x = tf.keras.layers.Dropout(0.2)(x)
-outputs = prediction_layer(x)
-model = tf.keras.Model(inputs, outputs)
-
-
-model.summary()
-
-len(model.trainable_variables)
-
-
-base_learning_rate = 0.0001
-model.compile(
-    optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),
-    loss=tf.keras.losses.BinaryCrossentropy(),
-    metrics=[tf.keras.metrics.BinaryAccuracy(threshold=0.5, name="accuracy")],
-)
-
-initial_epochs = 10
-
-
-loss0, accuracy0 = model.evaluate(validation_dataset)
-
-print("initial loss: {:.2f}".format(loss0))
-print("initial accuracy: {:.2f}".format(accuracy0))
-
-
-history = model.fit(
-    train_dataset, epochs=initial_epochs, validation_data=validation_dataset
-)
-
-acc = history.history["accuracy"]
-val_acc = history.history["val_accuracy"]
-
-loss = history.history["loss"]
-val_loss = history.history["val_loss"]
-
-plt.figure(figsize=(8, 8))
-plt.subplot(2, 1, 1)
-plt.plot(acc, label="Training Accuracy")
-plt.plot(val_acc, label="Validation Accuracy")
-plt.legend(loc="lower right")
-plt.ylabel("Accuracy")
-plt.ylim([min(plt.ylim()), 1])
-plt.title("Training and Validation Accuracy")
-
-plt.subplot(2, 1, 2)
-plt.plot(loss, label="Training Loss")
-plt.plot(val_loss, label="Validation Loss")
-plt.legend(loc="upper right")
-plt.ylabel("Cross Entropy")
-plt.ylim([0, 1.0])
-plt.title("Training and Validation Loss")
-plt.xlabel("epoch")
-plt.show()
+# Decode the predicted class (replace with your class labels)
+predicted_class = np.argmax(predictions[0])
+print(f"Predicted class: {predicted_class}")
