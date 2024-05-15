0965e6fbf8010ff1a4738526d30b57f37accdadb
Simone Poggiali <gibbok.coding@gmail.com>
2024-05-15 15:21:41 +0200

    s

--- experiments/project2/README.md
+++ experiments/project2/README.md
@@ -4,12 +4,35 @@ Given an historical collection of expenditures, and a year in the future; foreca
 
 ## Input
 
-A collection of expenditure.
+A collection of expenditure in CZK.
 
 ## Output
 
-A single value with the forecasted expenditure amount.
+A single value with the forecasted expenditure amount in CZK and EUR.
 
 ## Result
 
 ![result](./assets/result.png)
+
+```
+Predicted outcome for year 2024: [103729.2195] CZK or [4184.22252261] EUR
+Predicted outcome per month is [8644.101625] CZK or [348.68521022] EUR
+Predicted using the trained model:
+    years       outcome
+0    2008  21388.479265
+1    2009  26534.775529
+2    2010  31681.071794
+3    2011  36827.368059
+4    2012  41973.664324
+5    2013  47119.960588
+6    2014  52266.256853
+7    2015  57412.553118
+8    2016  62558.849382
+9    2017  67705.145647
+10   2018  72851.441912
+11   2019  77997.738176
+12   2020  83144.034441
+13   2021  88290.330706
+14   2022  93436.626971
+15   2023  98582.923235
+```
--- experiments/project2/project2.py
+++ experiments/project2/project2.py
@@ -1,5 +1,5 @@
 """
-Calculate Food Expenditures based on historical data.
+Calculate expenditures based on historical data.
 The result are reported in both CZK and EUR currencies.
 """
 
@@ -24,10 +24,10 @@ data = pd.read_csv("./experiments/project2/data.csv")
 
 # Get years as positive numbers
 years = data["years"].abs()
-# Get food_cost as positive numbers, and fill missing values with average from prev and next rows
-food_cost = data["outcome"].abs().interpolate(method="linear")
+# Get outcome as positive numbers, and fill missing values with average from prev and next rows
+outcome = data["outcome"].abs().interpolate(method="linear")
 
-data_sanitized = pd.DataFrame({"years": years, "outcome": food_cost})
+data_sanitized = pd.DataFrame({"years": years, "outcome": outcome})
 
 X = data_sanitized[["years"]]  # Features (independent variable)
 y = data_sanitized["outcome"]  # Target (dependent variable)
@@ -46,16 +46,16 @@ predicted_y_month = predicted_y / 12
 predicted_y_month_eur = predicted_y_month / czk_rate
 
 print(
-    f"Predicted food_share for year {new_year}: {predicted_y} CZK or {predicted_y_eur} EUR"
+    f"Predicted outcome for year {new_year}: {predicted_y} CZK or {predicted_y_eur} EUR"
 )
 print(
-    f"Predicted food_share per month is {predicted_y_month} CZK or {predicted_y_month_eur} EUR"
+    f"Predicted outcome per month is {predicted_y_month} CZK or {predicted_y_month_eur} EUR"
 )
 
 # Predict using the trained model
 predicted_y_trained = model.predict(X)
 predicted_y_trained_df = pd.DataFrame({"years": years, "outcome": predicted_y_trained})
-print("Predicted using the trained model")
+print("Predicted using the trained model:")
 print(predicted_y_trained_df)
 
 # Plotting the regression line in a new window
